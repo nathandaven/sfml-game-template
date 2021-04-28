8341699258b94a0cffea5214f95cff501a3816bb
Nathan Davenport <nathandaven@gmail.com>
2021-04-28 18:29:02 -0400

    Updated the sample test

--- test/test_RenderWindow.cpp
+++ test/test_RenderWindow.cpp
@@ -1,6 +1,6 @@
 #include <catch2/catch.hpp>
 
-// This example is a bit silly, but you get the idea
+// I made my own silly test! Enjoy
 TEST_CASE("sf::RenderWindow", "[renderwindow]")
 {
 	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
@@ -16,17 +16,30 @@ TEST_CASE("sf::RenderWindow", "[renderwindow]")
 	REQUIRE(static_cast<int>(shape.getLocalBounds().width) == 198); // 🤔
 	REQUIRE(static_cast<int>(shape.getLocalBounds().height) == 200);
 
-	sf::Texture shapeTexture;
-	shapeTexture.loadFromFile("content/sfml.png");
-	shape.setTexture(&shapeTexture);
+	sf::Font font;
+	font.loadFromFile("Content/joystix.ttf");
 
-	REQUIRE(shapeTexture.getSize().x == 256);
-	REQUIRE(shapeTexture.getSize().y == 256);
-	REQUIRE(shape.getTexture() == &shapeTexture);
+	sf::Text text;
+
+	// select the font
+	text.setFont(font); // font is a sf::Font
+
+	// set the string to display
+	text.setString("Nice!");
+
+	// set the character size
+	text.setCharacterSize(12); // in pixels, not points!
+
+	// set the color
+	text.setFillColor(sf::Color::White);
+
+	// set the text style
+	REQUIRE(text.getCharacterSize() == 12);
+	REQUIRE(text.getFont() == &font);
 
 	// Show the RenderWindow once
 	window.clear();
-	window.draw(shape);
+	window.draw(text);
 	window.display();
 
 	REQUIRE(window.isOpen() == true);
